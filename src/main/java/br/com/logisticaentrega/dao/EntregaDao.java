1a2721114039dde07a71a4e107d9c74dd944509b
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2025-09-11 17:54:31 -0300

    feat: implementa select em EntregaDao

--- src/main/java/br/com/logisticaentrega/dao/EntregaDao.java
+++ src/main/java/br/com/logisticaentrega/dao/EntregaDao.java
@@ -1,14 +1,19 @@
 package br.com.logisticaentrega.dao;
 
+import br.com.logisticaentrega.model.Entrega;
 import br.com.logisticaentrega.model.Motorista;
 import br.com.logisticaentrega.model.Pedido;
 import br.com.logisticaentrega.model.StatusEntrega;
+import br.com.logisticaentrega.service.ServiceCRUD;
 import br.com.logisticaentrega.util.Conexao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EntregaDao{
 
@@ -35,5 +40,41 @@ public class EntregaDao{
         e.printStackTrace();
     }
 
+    }
+
+    public List<Entrega> select(){
+        String sql = """
+                SELECT id, pedido_id, motorista_id, data_saida, data_entrega, status_entrega
+                FROM historicoEntrega;
+                """;
+
+        List<Entrega> entregas = new ArrayList<>();
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            ServiceCRUD crud = new ServiceCRUD();
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+                Integer id = rs.getInt("id");
+                Integer pedido_id = rs.getInt("pedido_id");
+                Integer motorista_id = rs.getInt("motorista_id");
+                LocalDateTime data_saida = rs.getObject("data_saida", LocalDateTime.class);
+                LocalDateTime data_entrega = rs.getObject("data_entrega", LocalDateTime.class);
+                String status = rs.getString("status_entrega");
+
+                Pedido pedido = crud.listagemId_pedido(pedido_id);
+                Motorista motorista = crud.listagemId_motorista(motorista_id);
+
+                var entrega = new Entrega(id, pedido, motorista, data_saida, data_entrega, status);
+                entregas.add(entrega);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return entregas;
     }
 }
